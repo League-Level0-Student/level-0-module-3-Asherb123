0791a50e5d3752a0da932dd0a75730fea89c38f2
League <league@WTS8.attlocal.net>
2019-04-22 18:28:06 -0700

    I did some stuff. :unamused:

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,16 +5,28 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
+		
+		String s = JOptionPane.showInputDialog("How many cats do you have?");
+		
 		// 2. Convert their answer into an int
-
+		int cats=Integer.parseInt(s);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
-
+		if (cats >= 3) {
+		JOptionPane.showMessageDialog(null, "Why come no seafood?......... AKA YOU'RE WEIRD.");
+		} else if(cats< 3 && cats >0) {
+		JOptionPane.showMessageDialog(null, ("NOOOOOO UUUUU!!!!"));
+		 playVideo( "https://www.youtube.com/watch?v=9EMGlrB-VPs");
+		
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-
+		if (cats<= 0) {
+		playVideo("https://www.youtube.com/watch?v=9Deg7VrpHbM");
+		}
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
 	}
